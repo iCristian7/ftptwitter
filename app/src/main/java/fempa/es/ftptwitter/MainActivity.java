2060c6e2f2d2b476e3c797f2e732c8f3981518fc
Cristian <cristianperezsegura@gmail.com>
2018-01-22 18:29:16 +0100

    Conexion a FTP

--- app/src/main/java/fempa/es/ftptwitter/MainActivity.java
+++ app/src/main/java/fempa/es/ftptwitter/MainActivity.java
@@ -1,7 +1,15 @@
 package fempa.es.ftptwitter;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.apache.commons.net.ftp.*;
+
+import java.io.IOException;
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +17,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-
     }
 
+    public void comprobarPuntuacion(View v)  {
+
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                FTPClient cliente = null;
+                String usuarioFTP = "u354500985.fempaalumnos";
+                String passFTP = "AlumnosFempa";
+
+                try {
+                    cliente = new FTPClient();
+                    cliente.setConnectTimeout(10 * 1000);
+                    cliente.connect(InetAddress.getByName("31.170.165.160"));
+                    boolean login = cliente.login(usuarioFTP,passFTP);
+                    if(login){
+                        Log.e("login","Logeado");
+                        Toast.makeText(MainActivity.this, "Logeado", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Log.e("login","no Logeado");
+                        Toast.makeText(MainActivity.this, "Error al logearse", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                try {
+                    cliente.disconnect();
+                    //cliente.logout();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }).start();
+
+    }
 
 }
